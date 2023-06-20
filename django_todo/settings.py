8c81bd933429dda2a952f0f4301f10d011cdbe88
John Clarke <jp_clarke@outlook.com>
2023-06-20 21:59:47 +0000

    Correct database_url and add secret_key to config vars

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if os.path.exists("env.py"):
+if os.path.isfile('env.py'):
     import env
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
